df3dcb266c1fbc543b4ce69f3da6de84916513f2
Dennis Mayeku <dennismayeku@gmail.com>
2023-09-05 19:29:16 +0300

    updates

--- docker/docker-entrypoint-init.d/00-swoole.sh
+++ docker/docker-entrypoint-init.d/00-swoole.sh
@@ -3,12 +3,13 @@
 # Make sure to chmod a+x on this file so it'll run during Bitnami's setup.
 
 # Install the dependencies
-#apt-get -y update
+apt-get -y update
 apt-get -y install autoconf unzip
 apt-get -y install build-essential
 
 # Set PECL defaults
-pecl channel-update
+#pecl channel-update
+pecl channel-update pecl.php.net
 pear config-set php_ini /opt/bitnami/php/etc/php.ini
 pecl config-set php_ini /opt/bitnami/php/etc/php.ini
 
